e6a6278e57d456927f44e4fe57ea7de23d9a7c25
ines1900 <103032627+ines1900@users.noreply.github.com>
2022-08-16 16:16:19 +0100

    Update RunCukesTest.java

--- src/test/java/runcukes/RunCukesTest.java
+++ src/test/java/runcukes/RunCukesTest.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/Features", strict=true , glue="stepdefs" , tags="@scenario",
-        		plugin = {"pretty","html:target/HtmlReports"}
+        		plugin = {"pretty","html:target/HtmlReports","json:target/ines.json"}
 		)
 
 public class RunCukesTest {
